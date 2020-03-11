857cb43633b1588dae8c7be99cdda1945c625d95
q55mehqs <eviyuyu14@gmail.com>
2020-03-11 23:48:54 +0900

    update credit check function
    単位情報を科目表に近い形に表示させる関数の空白埋めを改良

--- functions/check_syllabus_data.py
+++ functions/check_syllabus_data.py
@@ -1,17 +1,33 @@
 from json import loads, dumps
 from os import path
+import unicodedata
+
+def left(digit: int, msg: str) -> str:
+    for c in msg:
+        if unicodedata.east_asian_width(c) in ('F', 'W', 'A'):
+            digit -= 2
+        else:
+            digit -= 1
+    return msg + ' ' * digit
+
 
 raw = {}
 with open(path.join(path.dirname(__file__) ,"../jugyoListCredit_17s-.json"), "r", encoding="utf-8") as f:
     raw = loads(f.read())
 
-checklist = ["IS4", "IT4", "IE4", "IS5", "IT5", "IE5"]
+checklist = [
+    "1-1", # "1-2", "1-3", # 1年は同じなので省略
+    "IS2", "IT2", "IE2",
+    "IS3", "IT3", "IE3",
+    "IS4", "IT4", "IE4",
+    "IS5", "IT5", "IE5"
+]
 
 for c in checklist:
     print(c)
     for subj, v in raw[c].items():
         ippan = "専門" if v["専門"] else "一般"
         hisshu = "必修" if v["必修"] else "選択"
-        gakushu_circle = "○\t" if v["学修"] else "\t"
-        print("%s||%s|%20s|%s|%d" % (ippan, hisshu, subj, gakushu_circle, v["単位数"]))
+        gakushu_circle = "○   " if v["学修"] else "    "
+        print("%s||%s|%s|%s|%d" % (ippan, hisshu, left(40, subj), gakushu_circle, v["単位数"]))
         input()
